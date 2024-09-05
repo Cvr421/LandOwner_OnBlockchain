42ffcf05e30cb3ea5aaf2af75cdc40d828a95329
Cvr421 <chandravijayk42187@gmail.com>
2024-09-05 23:36:45 +0530

    Implement listing and deposits test

--- contracts/Escrow.sol
+++ contracts/Escrow.sol
@@ -9,11 +9,32 @@ interface IERC721 {
     ) external;
 }
 
+
+
 contract Escrow {
     address public nftAddress;
     address payable public seller;
     address public inspector;
     address public lender ;
+
+modifier onlyBuyer(uint256 _nftID){
+  require(msg.sender==buyer[_nftID],"only buyer can call this method");
+  _;
+}
+  modifier onlySeller(){
+    require(msg.sender==seller,"Only seller can call this method");
+    _;
+  }
+
+
+      //NFT index //this is the value for property for listed or not (true or false )
+mapping(uint256 => bool) public isListed;
+mapping(uint256 => uint256) public purchasePrice;
+mapping(uint256 => uint256) public escrowAmount;
+mapping(uint256 => address) public buyer;
+
+
+
 
 
   constructor (address _nftAddress, address payable _seller, address _inspector, address _lender){
@@ -22,5 +43,33 @@ contract Escrow {
     inspector=_inspector;
     lender=_lender;
   }
-   
+   function list(uint256 _nftID, address _buyer, uint256 _purchasePrice, uint256 _escrowAmount)public payable onlySeller{
+    // Transfer NFT from seller to this contract 
+    IERC721(nftAddress).transferFrom(msg.sender, address(this), _nftID);
+
+    isListed[_nftID]= true;
+    purchasePrice[_nftID]=_purchasePrice;
+    escrowAmount[_nftID]=_escrowAmount;
+    buyer[_nftID]=_buyer;
+
+
+
+   }
+  // put under Contract (only buyer - payable escrow)
+   function depositEarnest(uint256 _nftID)public payable onlyBuyer(_nftID){
+    require(msg.value >= escrowAmount[_nftID]);
+
+   }
+
+receive() external payable{}
+
+
+// getting baclane of this contract
+   function getBalance() public view returns(uint256){
+    return address(this).balance;
+   }
+
+
+
+
 }
--- test/Escrow.js
+++ test/Escrow.js
@@ -1,5 +1,6 @@
 const { expect } = require('chai');
 const { ethers } = require('hardhat');
+const { experimentalAddHardhatNetworkMessageTraceHook } = require('hardhat/config');
 // const { experimentalAddHardhatNetworkMessageTraceHook } = require('hardhat/config');
 
 const tokens = (n) => {
@@ -33,10 +34,21 @@ describe('Escrow', () => {
         inspector.address,
         lender.address
     )
+
+    // Approve property
+    transaction = await realEstate.connect(seller).approve(escrow.address,1)
+    await transaction.wait()
+
+    // List property
+    transaction=await escrow.connect(seller).list(1,buyers.address,tokens(10),tokens(5))
+    await transaction.wait()
+   
+
+
   })
 
 
-   describe('Deployment',()=>{
+  describe('Deployment',()=>{
 
         it('Returns NFT address',async()=>{
            const result=await escrow.nftAddress()
@@ -60,6 +72,50 @@ describe('Escrow', () => {
            expect(result).to.be.equal(lender.address)
         })
        
-    })
+  })
+
+  describe('Listing',()=>{
     
+      it('Update as listed', async()=>{
+        const result=await escrow.isListed(1)
+        expect(result).to.be.equal(true)
+      })
+    
+
+      it('Updating ownership', async()=>{
+        expect (await realEstate.ownerOf(1)).to.be.equal(escrow.address)
+      })
+
+      it('Returns buyer', async()=>{
+        const result =await escrow.buyer(1)
+        expect(result).to.be.equal(buyers.address)
+      })
+
+      it('Returns purchase price ', async()=>{
+       const result =await escrow.purchasePrice(1)
+       expect(result).to.be.equal(tokens(10))
+      })
+
+      it('Returns escrow amount', async()=>{
+       const result =await escrow.escrowAmount(1)
+       expect(result).to.be.equal(tokens(5))
+    })
+  }) 
+
+
+
+ describe('Deposits',()=>{
+  it('Updates contract balance',async ()=>{
+   const transaction= await escrow.connect(buyers).depositEarnest(1,{value: tokens(5)})
+   await transaction.wait()
+   const result =await escrow.getBalance()
+   expect(result).to.be.equal(tokens(5))
+    
+  })
+})
+
+
+
+
+
 })
