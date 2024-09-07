872b5ecb05f41fbd7a8663278673649ee6d44604
Cvr421 <chandravijayk42187@gmail.com>
2024-09-07 21:53:15 +0530

    Implement all the backend Blockchain contract (RealEstate,Escrow)

--- test/Escrow.js
+++ test/Escrow.js
@@ -104,14 +104,80 @@ describe('Escrow', () => {
 
 
 
- describe('Deposits',()=>{
-  it('Updates contract balance',async ()=>{
+  describe('Deposits',()=>{
+    it('Updates contract balance',async ()=>{
    const transaction= await escrow.connect(buyers).depositEarnest(1,{value: tokens(5)})
    await transaction.wait()
    const result =await escrow.getBalance()
    expect(result).to.be.equal(tokens(5))
     
+   })
   })
+
+  describe('Inspection',()=>{
+    it('Updates contract balance',async ()=>{
+   const transaction= await escrow.connect(inspector).updateInspectorStatus(1,true)
+   await transaction.wait()
+   const result =await escrow.inspectionPassed(1)
+   expect(result).to.be.equal(true)
+    
+   })
+  })
+
+  describe('Approval',()=> {
+    it('Updates approval status',async ()=>{
+      let transaction =await escrow.connect(buyers).approveSale(1)
+      await transaction.wait()
+
+      transaction=await escrow.connect(seller).approveSale(1)
+      await transaction.wait()
+
+      transaction=await escrow.connect(lender).approveSale(1)
+      await transaction.wait()
+
+      expect(await escrow.approval(1,buyers.address)).to.be.equal(true)
+      expect(await escrow.approval(1,seller.address)).to.be.equal(true)
+      expect(await escrow.approval(1,lender.address)).to.be.equal(true)
+   })
+
+  })
+
+describe('Sale',async()=>{
+  beforeEach(async()=>{
+    let transaction=await escrow.connect(buyers).depositEarnest(1,{value:tokens(5)})
+    await transaction.wait()
+
+    transaction=await escrow.connect(inspector).updateInspectorStatus(1,true)
+    await transaction.wait()
+
+    transaction=await escrow.connect(buyers).approveSale(1)
+    await transaction.wait()
+
+    transaction=await escrow.connect(seller).approveSale(1)
+    await transaction.wait()
+
+    transaction=await escrow.connect(lender).approveSale(1)
+    await transaction.wait()
+
+    await lender.sendTransaction({to: escrow.address, value:tokens(5)})
+    transaction =await escrow.connect(seller).finalizeSale(1)
+    await transaction.wait()
+
+    
+
+
+  })
+
+  it('Updates ownership', async()=>{
+   expect (await realEstate.ownerOf(1)).to.be.equal(buyers.address)
+
+
+  })
+   it('Updates balance',async()=>{
+  expect(await escrow.getBalance()).to.be.equal(0)
+   })
+
+
 })
 
 
